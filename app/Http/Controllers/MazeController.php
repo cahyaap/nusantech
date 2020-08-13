9ba56c41b6f50a92d5c2acd53fde7f0ea9c508b6
cahya <cahyaajipermana@gmail.com>
2020-08-13 09:17:08 +0700

    maze generator pola pertama

---
+++ app/Http/Controllers/MazeController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MazeController extends Controller
+{
+    public function index()
+    {
+        return view('pages.maze');
+    }
+
+    public function generate(Request $request)
+    {
+        $maze = "";
+
+        $s = $request->input('suku');
+
+        if($this->checkInput($s)){
+            return response()->json([
+                'maze' => $maze,
+                's' => $s
+            ]);
+        }
+
+        // buat polanya dinding pintu kiri, jalan, sama dindin pintu kanan
+        $dindingPintuKiri = "";
+        $jalan = "";
+        $dindingPintuKanan = "";
+    
+        $lastIndex = $s-1;
+        $pintuKanan = $lastIndex-1;
+      
+        for($i=0;$i<$s;$i++){
+            if($i == 1){
+                  $dindingPintuKiri = $dindingPintuKiri."<pre>&nbsp;</pre>";
+            }else{
+                  $dindingPintuKiri = $dindingPintuKiri."<pre>@</pre>";  
+            }
+    
+            if($i == 0 || $i == $lastIndex){
+                  $jalan = $jalan."<pre>@</pre>";    
+            }else{
+                  $jalan = $jalan."<pre>&nbsp;</pre>";  
+            }
+    
+            if($i == $pintuKanan){
+                  $dindingPintuKanan = $dindingPintuKanan."<pre>&nbsp;</pre>";
+            }else{
+                  $dindingPintuKanan = $dindingPintuKanan."<pre>@</pre>";  
+            }
+        }
+        // end pola dinding
+    
+        // buat pola mazenya
+        $maze = "";
+        $kiriStatus = true;
+        $jalanStatus = false;
+        $kananStatus = false;
+        $kiriCounter = 0;
+        $kananCounter = 0;
+        
+        for($j=0;$j<$s;$j++){
+            $maze = $maze."<span class='maze-row'>";
+            if($kiriStatus){
+                $maze = $maze.$dindingPintuKiri;
+                $kiriStatus = false;
+                $jalanStatus = true;
+                $kiriCounter++;
+            } else if($jalanStatus){
+                $maze = $maze.$jalan;
+                if($kiriCounter == $kananCounter){
+                    $kiriStatus = true;
+                } else {
+                    $kananStatus = true;	
+                }
+                $jalanStatus = false;
+            } else if($kananStatus){
+                $maze = $maze.$dindingPintuKanan;
+                $kananStatus = false;
+                $jalanStatus = true;
+                $kananCounter++;
+            }
+            $maze = $maze."</span>";
+        }
+        // end pola maze
+
+        return response()->json([
+            'maze' => $maze,
+            's' => $s
+        ]);
+    }
+
+    public function checkInput($s)
+    {
+        $n = ($s+1)/4;
+        if(!preg_match('/^\d+$/', $n)){
+            return true;
+        }
+        return false;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'MazeController@index');
+Route::post('/generate', 'MazeController@generate')->name('generate');
